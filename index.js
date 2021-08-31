0a26adcbc5bb3a49e1849751e31b57ec5887d59e
saycubed <76540140+saycubed@users.noreply.github.com>
2021-09-01 02:24:56 +0800

    Update index.js

--- index.js
+++ index.js
@@ -692,6 +692,10 @@ bot.action('empty', (ctx) =>{
         }
     })
 })
+var port = process.env.PORT || 3000;
+app.listen(port, "0.0.0.0", function() {
+console.log("Listening on Port 3000");
+});
 
 ;(async function () {
     try {
